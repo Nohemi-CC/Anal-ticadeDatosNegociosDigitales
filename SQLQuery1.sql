a1c419deed9dc6067485d5c7e6e6dbc58d0c2ab0
Nohemi <nomy061004@gmail.com>
2025-05-22 12:24:04 -0600

    Logro: Inicio del Repositorio para Analítica de Datos para Negocios Digitales

---
+++ SQLQuery1.sql
@@ -0,0 +1,127 @@
+-- Create la base de datos sde Stage_Northwind
+
+create database stage_northwind
+
+--Crear la base de datos datamart_Northwind
+create database datamart_nortwind
+
+-- Implementar la base datos stage_northwind
+ use stage_northwind 
+
+ create table categorias(
+ categoriaid int not null,
+ nombrecategoria varchar(15)
+ );
+
+ create table cliente(
+ clienteid char(5) not null,
+ compania varchar(40) not null,
+ ciudad varchar(15),
+ region varchar(15),
+ codigopostal char(10),
+ country nvarchar(15)
+ );
+
+ create table empleados(
+ empleadoid int not null,
+ nombre varchar(10) not null,
+ apellido varchar(20) not null,
+ fecha_contratacion date
+ );
+
+  create table producto(
+  productoid int not null,
+  nombreproducto varchar (50) not null,
+  preciounitario decimal(15,2) not null,
+  );
+
+  create table proveedor(
+  proveedorid int not null,
+  proveedornombre varchar(40) not null,
+  ciudad varchar (15),
+  region varchar (15),
+  pais varchar (15)
+  );
+
+  create table ventas(
+  clienteid char (5) not null,
+  empleadocodigo int not null,
+  productocodigo int not null,
+  ventasorden datetime not null,
+  ventasmonto decimal(15,2) not null,
+  ventasunidades int not null,
+  ventaspreciounitario decimal(15,2),
+  ventasdescuento decimal(15,2) not null
+  );
+
+  -- Crear datamart_northwind
+
+  use datamart_nortwind
+
+  create table dim_cliente(
+  cliente_Skey int not null identity(1,1),
+  cliente_codigoBkey char(5) not null,
+  cliente_compania varchar(40) not null,
+  cliente_ciudad varchar(15) not null,
+  cliente_region varchar(25) not null,
+  cliente_pais varchar(15) not null,
+  constraint pk_dimcliente
+  primary key(cliente_Skey)
+  );
+
+  create table dim_empleado(
+  empleado_SKey int not null identity(1,1),
+  empleado_codigoBKey int not null,
+  empleado_NombreCompleto varchar(100) not null,
+  constraint pk_dimempleado
+  primary key(empleado_SKey)
+  );
+
+  create table dim_producto(
+  producto_SKey int not null identity(1,1),
+  producto_CodigoBkey int not null,
+  producto_Nombre varchar(80) not null,
+  producto_Categoria_nombre varchar(15) not null
+  constraint pk_dimproducto
+  primary key(producto_SKey)
+  );
+
+  create table dim_tiempo(
+  tiempo_SKey int not null identity(1,1),
+  tiempo_fechaActual datetime not null,
+  tiempo_anio int not null,
+  tiempo_trimestre int not null,
+  tiempo_mes int not null,
+  tiempo_semana int not null,
+  tiempo_diadeanio int not null,
+  tiempo_diademes int not null,
+  tiempo_diasemana int not null,
+  constraint pk_dimtiempo
+  primary key(tiempo_SKey)
+  );
+
+  create table fact_ventas(
+  cliente_SKey int not null,
+  empleado_SKey int not null,
+  producto_Skey int not null,
+  tiempo_SKey int not null,
+  ventas_Norden int not null,
+  ventas_monto decimal(15,2) not null,
+  ventas_unidades int not null,
+  ventas_punitario decimal (15,2) not null,
+  ventas_descuento decimal(15,2) not null,
+  constraint pk_factventas
+  primary key (cliente_SKey,empleado_SKey,producto_Skey,tiempo_SKey),
+  constraint fk_factventas_dimcliente
+  foreign key (cliente_SKey)
+  references dim_cliente(cliente_SKey),
+  constraint fk_factventas_dimempleado
+  foreign key (empleado_SKey)
+  references dim_empleado(empleado_SKey),
+  constraint fk_factventas_dimproducto
+  foreign key (producto_Skey)
+  references dim_producto(producto_Skey),
+  constraint fk_factventas_dimtiempo
+  foreign key (tiempo_SKey)
+  references dim_tiempo(tiempo_SKey)
+  );
